4e57b16d96e3c289aab4cc1bcfd7e13cc71c93ce
Thomas Van <thomascvan11@gmail.com>
2023-01-31 18:10:31 -0500

    twoSum solution

--- 1-array/1-two-sum.js
+++ 1-array/1-two-sum.js
@@ -37,5 +37,19 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+  var hashMap = {};
 
+  for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
+      let complement = target - nums[i];
+      if (hashMap[complement] !== undefined) {
+        return [hashMap[complement], i];
+      }
+      hashMap[nums[i]] = i;
+    }
+  }
 };
+
+console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([3,2,4], 6));
+console.log(twoSum([3,3], 6));

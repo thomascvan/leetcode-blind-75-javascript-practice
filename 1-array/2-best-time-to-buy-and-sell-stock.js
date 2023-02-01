14b94163cda746fbb9487c2dc8a56b735c16dcbf
Thomas Van <thomascvan11@gmail.com>
2023-02-01 14:02:50 -0500

    added test cases

--- 1-array/2-best-time-to-buy-and-sell-stock.js
+++ 1-array/2-best-time-to-buy-and-sell-stock.js
@@ -31,3 +31,9 @@
 var maxProfit = function(prices) {
 
 };
+
+console.log('should return 5:', maxProfit([7,1,5,3,6,4]));
+console.log('should return 0:', maxProfit([7,6,4,3,1]));
+console.log('should return 2:', maxProfit([2,1,2,1,0,1,2]));
+console.log('should return 0:', maxProfit([7,6,4,3,1]));
+console.log('should return 2:', maxProfit([2,4,1]));

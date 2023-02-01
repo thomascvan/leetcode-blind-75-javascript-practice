adcd3ca222a001f44950f511f9cab9307b6b3fb4
Thomas Van <thomascvan11@gmail.com>
2023-02-01 14:04:45 -0500

    reformated test cases for two-sum

--- 1-array/1-two-sum.js
+++ 1-array/1-two-sum.js
@@ -50,6 +50,6 @@ var twoSum = function(nums, target) {
   }
 };
 
-console.log(twoSum([2,7,11,15], 9));
-console.log(twoSum([3,2,4], 6));
-console.log(twoSum([3,3], 6));
+console.log('should return [0,1]:', twoSum([2,7,11,15], 9));
+console.log('should return [1,2]:', twoSum([3,2,4], 6));
+console.log('should return [0,1]:', twoSum([3,3], 6));

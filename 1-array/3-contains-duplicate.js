74ba7be1fd3976d258869b6ca6286ac009789e7a
Thomas Van <thomascvan11@gmail.com>
2023-05-23 15:32:52 -0700

    contains-duplicates solution

--- 1-array/3-contains-duplicate.js
+++ 1-array/3-contains-duplicate.js
@@ -19,6 +19,21 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
 
+var containsDuplicate = function(nums) {
+  let hashmap = {};
+
+  for (let number of nums) {
+    if (!hashmap[number]) {
+      hashmap[number] = 1;
+    }
+    else return true;
+  }
+
+  return false;
 };
+
+console.log(containsDuplicate([1,2,3,1]))
+console.log(containsDuplicate([1,2,3]))
+console.log(containsDuplicate([1,2,3,4]))
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
